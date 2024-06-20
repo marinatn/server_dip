625c523d0e0c34ae4fd62b86943e671500bdbc02
Sergei Trizna <init.reg@gmail.com>
2024-06-21 01:02:31 +0300

    cors middleware

--- med-server/bootstrap/app.php
+++ med-server/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\OwnCors;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,6 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
 //        health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(OwnCors::class);
+
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
